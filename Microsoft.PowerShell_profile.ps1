c7a42b78ec096279e6ac9f139defc47b5977b830
Zach Carrington <zcarrington@gmail.com>
2020-03-14 11:15:35 -0500

    PS profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+﻿
+Import-Module posh-git
+
+Import-Module DockerCompletion
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
